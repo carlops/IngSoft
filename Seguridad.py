73eae5558c1f5a9566604d06c1c6db1d3e34838c
Carlo Polisano S <carlo.polisano@gmail.com>
2014-10-02 01:15:35 -0430

    Funcionan ambas funciones(registrar y autenticar), faltan crear mas casos de prueba para el TDD

--- Seguridad.py
+++ Seguridad.py
@@ -10,7 +10,9 @@ class clsSeguridad(object):
     '''
     classdocs
     '''
-    datos={}
+    ERcorreo = re.compile(r'\w(\w|_|\.|-)*@\w+\.\w+')
+    ERclave = re.compile(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-z]){3,15}.{8,16}$')
+    Diccionario = {}
     def __init__(self):
         '''
         Constructor
@@ -19,19 +21,43 @@ class clsSeguridad(object):
     def registrarUsuario(self,correo,clave1,clave2):
         correoValido=False
         claveValida=False
-        if (re.match(r'\w(\w|_|\.|-)*@\w+\.\w+',correo)):
+        if (isinstance(correo,str)and(self.ERcorreo.match(correo))):
+            #(re.match(r'\w(\w|_|\.|-)*@\w+\.\w+',correo)):
             correoValido=True
         else:
             print ("Dirección de correo electrónico inválido")
         
         if ((isinstance(clave1, str))and(isinstance(clave2, str))and(clave1==clave2)):
-            if re.match(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-z]){3,15}.{8,16}$', clave1):
+            if self.ERclave.match(clave1):
                 claveValida=True
         if not (claveValida):
             print("Clave inválida")
             
         if (claveValida and correoValido):
             claveCodificada=clave1[::-1]
-            self.datos[correo]=claveCodificada;
-            print(self.datos)
-            # meter en el diccionario
+            self.Diccionario[correo]=claveCodificada;
+#            print(self.Diccionario)
+            
+    def AutenticarUsuario(self,correo,clave):
+        correoValido=False
+        claveValida=False
+        print(self.Diccionario)
+        print(correo)
+        print(correo in self.Diccionario)
+        if isinstance(correo,str) and self.ERcorreo.match(correo) and (correo in self.Diccionario):
+            correoValido= True
+            claveCodificada = self.Diccionario[correo]
+            claveCodificada = claveCodificada[::-1]
+            
+            if isinstance(clave,str) and self.ERclave.match(clave) and claveCodificada==clave:
+                claveValida = True
+
+        if claveValida and correoValido:
+            print("Usuario Aceptado")
+
+        if correoValido and not claveValida:
+            print("Clave invalida")
+
+        if not correoValido:
+            print("Correo invalido")
+        print()
--- testSeguridad.py
+++ testSeguridad.py
@@ -11,12 +11,25 @@ seg=clsSeguridad()
 
 class Test(unittest.TestCase):
 
-    def testRegistroIni(self):
-        seg.registrarUsuario()
+#    def testRegistroIni(self):
+#        seg.registrarUsuario()
 
     def testRegistroBien(self): 
         seg.registrarUsuario("jon@blahmail.com","a12345F6789fg","a12345F6789fg")
 
+    def testAutBien(self):
+        seg.registrarUsuario("jon1@blahmail.com","a12345F6789fg","a12345F6789fg")
+        seg.AutenticarUsuario("jon1@blahmail.com","a12345F6789fg")
+        
+    def testAutMalaClave(self):
+        seg.registrarUsuario("jon2@blahmail.com","a12345F6789fg","a12345F6789fg")
+        seg.AutenticarUsuario("jon2@blahmail.com","a12345F6789f")
+        
+    def testAutMalCorreo(self):
+        seg.registrarUsuario("jon3@blahmail.com","a12345F6789fg","a12345F6789fg")
+        seg.AutenticarUsuario("jon3@mail.com","a12345F6789fg")
+        
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
